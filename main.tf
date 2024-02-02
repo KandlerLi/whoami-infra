ced8634e64c57715a8ad87c8d4befd01d2ca9f0e
Julian Kandler <julian.kandler@outlook.com>
2024-02-02 12:09:50 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "~> 1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = {
+      CreatedBy = "terraform"
+      Project   = "whoami-infra"
+    }
+  }
+}
+
+
+resource "aws_s3_bucket" "state_file" {
+  bucket = "kandler-li-test-bucket"
+}
