3ad36f0355497aa53a754983873100ec877a5ad1
Julian Kandler <julian.kandler@outlook.com>
2024-02-02 14:16:19 +0100

    Change S3 to public

--- main.tf
+++ main.tf
@@ -25,7 +25,25 @@ terraform {
   }
 }
 
-
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "whoami_ui" {
   bucket = "kandler-li-whoami-ui"
+}
+
+resource "aws_s3_bucket_website_configuration" "config" {
+  bucket = aws_s3_bucket.whoami_ui.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
+
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket                  = aws_s3_bucket.whoami_ui.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
