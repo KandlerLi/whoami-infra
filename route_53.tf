2a6fc46f47ac5290edd88811c40a436fbfb903ae
Julian Kandler <julian.kandler@outlook.com>
2024-02-03 14:21:57 +0100

    Add root record

--- route_53.tf
+++ route_53.tf
@@ -13,3 +13,15 @@ resource "aws_route53_record" "www" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.jkandler_de.zone_id
+  name    = "jkandler.de"
+  type    = "A"
+
+  alias {
+    name                   = aws_s3_bucket_website_configuration.config.website_endpoint
+    zone_id                = aws_s3_bucket.whoami_ui.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
