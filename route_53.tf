e0175fc9ab1ba44c5fc3befc7f3ce908e0470f37
Julian Kandler <julian.kandler@outlook.com>
2024-02-03 20:26:18 +0100

    use static s3 endpoint for www

--- route_53.tf
+++ route_53.tf
@@ -8,7 +8,7 @@ resource "aws_route53_record" "www" {
   type    = "A"
 
   alias {
-    name                   = aws_s3_bucket_website_configuration.config.website_endpoint
+    name                   = "s3-website.eu-central-1.amazonaws.com" #aws_s3_bucket_website_configuration.config.website_endpoint
     zone_id                = aws_s3_bucket.whoami_ui.hosted_zone_id
     evaluate_target_health = true
   }
