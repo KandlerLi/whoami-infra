d90b60c26932aaf5fed153900126dec5eaaf5be2
Julian Kandler <julian.kandler@outlook.com>
2024-02-08 22:59:38 +0100

    Add ACM and Color

--- route_53.tf
+++ route_53.tf
@@ -2,18 +2,6 @@ resource "aws_route53_zone" "jkandler_de" {
   name = "jkandler.de"
 }
 
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.jkandler_de.zone_id
-  name    = "www.jkandler.de"
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.website_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.website_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
 resource "aws_route53_record" "root" {
   zone_id = aws_route53_zone.jkandler_de.zone_id
   name    = "jkandler.de"
@@ -61,6 +49,27 @@ resource "aws_cloudfront_distribution" "website_distribution" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = aws_acm_certificate.website_certificate.arn
+    ssl_support_method  = "sni-only"
   }
+}
+
+resource "aws_acm_certificate" "website_certificate" {
+  domain_name       = "jkandler.de"
+  validation_method = "DNS"
+}
+
+resource "aws_route53_record" "website_certificate_validation" {
+  count = length(aws_acm_certificate.website_certificate.domain_validation_options)
+
+  name    = element(aws_acm_certificate.website_certificate.domain_validation_options.*.resource_record_name, count.index)
+  type    = element(aws_acm_certificate.website_certificate.domain_validation_options.*.resource_record_type, count.index)
+  zone_id = aws_route53_zone.jkandler_de.zone_id
+  records = [element(aws_acm_certificate.website_certificate.domain_validation_options.*.resource_record_value, count.index)]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "website_certificate_validation" {
+  certificate_arn         = aws_acm_certificate.website_certificate.arn
+  validation_record_fqdns = aws_route53_record.website_certificate_validation.*.fqdn
 }
