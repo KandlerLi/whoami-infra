445caca70b2e677775e80bf3a8dc8a13e03e2643
Julian Kandler <julian.kandler@outlook.com>
2024-02-08 22:39:15 +0100

    Set up cloudfront

--- route_53.tf
+++ route_53.tf
@@ -8,9 +8,9 @@ resource "aws_route53_record" "www" {
   type    = "A"
 
   alias {
-    name                   = "s3-website.eu-central-1.amazonaws.com" #aws_s3_bucket_website_configuration.config.website_endpoint
-    zone_id                = aws_s3_bucket.whoami_ui.hosted_zone_id
-    evaluate_target_health = true
+    name                   = aws_cloudfront_distribution.website_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.website_distribution.hosted_zone_id
+    evaluate_target_health = false
   }
 }
 
@@ -20,8 +20,47 @@ resource "aws_route53_record" "root" {
   type    = "A"
 
   alias {
-    name                   = "s3-website.eu-central-1.amazonaws.com" #aws_s3_bucket_website_configuration.config.website_endpoint
-    zone_id                = aws_s3_bucket.whoami_ui.hosted_zone_id
-    evaluate_target_health = true
+    name                   = aws_cloudfront_distribution.website_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.website_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_cloudfront_distribution" "website_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id   = "S3-website"
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    target_origin_id = "S3-website"
+
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD", "OPTIONS"]
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
   }
 }
