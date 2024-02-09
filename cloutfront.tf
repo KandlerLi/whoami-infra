124e8f59bf805cfe8c5f3a73291a29dea0ccf7d1
Julian Kandler <julian.kandler@outlook.com>
2024-02-09 20:07:22 +0100

    Fix reference

--- cloutfront.tf
+++ cloutfront.tf
@@ -8,8 +8,8 @@ resource "aws_cloudfront_distribution" "cloudfront" {
   # Distributes content to US and Europe
   price_class = "PriceClass_100"
   origin {
-    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
-    origin_id   = "S3-${aws_s3_bucket.website.bucket}"
+    domain_name = aws_s3_bucket.whoami_ui.bucket_regional_domain_name
+    origin_id   = "S3-${aws_s3_bucket.whoami_ui.bucket}"
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
     }
@@ -23,7 +23,7 @@ resource "aws_cloudfront_distribution" "cloudfront" {
   default_cache_behavior {
     allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = "S3-${aws_s3_bucket.website.bucket}"
+    target_origin_id       = "S3-${aws_s3_bucket.whoami_ui.bucket}"
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
