2b11e925636e27bdd5aab46f3f402e1c17da5b2a
Julian Kandler <julian.kandler@outlook.com>
2024-02-14 10:19:40 +0100

    fix naming conflict

--- route_53.tf
+++ route_53.tf
@@ -2,7 +2,7 @@ resource "aws_route53_zone" "primary" {
   name = var.domain
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "root" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = var.domain
   type    = "A"
