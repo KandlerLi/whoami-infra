bdf69386440fa4539da5708e13a3aa428b9a6ea3
Julian Kandler <julian.kandler@outlook.com>
2024-02-14 10:54:30 +0100

    Task definition for whoami_api

--- ecs.tf
+++ ecs.tf
@@ -16,3 +16,42 @@ resource "aws_ecs_cluster" "whoami" {
 resource "aws_cloudwatch_log_group" "whoami_ecs" {
   name = "whoami_ecs"
 }
+
+resource "aws_cloudwatch_log_group" "whoami_api" {
+  name = "whoami_api"
+}
+
+
+resource "aws_ecs_task_definition" "default" {
+  family                   = "whoami_api"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_iam_role.arn
+  cpu                      = 0.25
+  memory                   = 512
+
+  container_definitions = jsonencode([
+    {
+      name      = "whoami_api"
+      image     = "${aws_ecr_repository.kandlerli.repository_url}:latest"
+      cpu       = 0.25
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+          protocol      = "tcp"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs",
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.whoami_api.name,
+          "awslogs-region"        = "eu-central-1",
+          "awslogs-stream-prefix" = "whoami_api-logs"
+        }
+      }
+    }
+  ])
+}
